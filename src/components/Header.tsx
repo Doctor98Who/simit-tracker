f861841fe35211dab78fc7fdd99351ce91397fe5
Doctor98Who <75399277+Doctor98Who@users.noreply.github.com>
2025-07-27 19:03:06 -0500

    123

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 interface HeaderProps {
   version?: string; // Optional prop for dynamic version
 }
-const Header: React.FC<HeaderProps> = ({ version = 'v0.0.20' }) => {
+const Header: React.FC<HeaderProps> = ({ version = 'v0.0.22' }) => {
   const [updateAvailable, setUpdateAvailable] = useState(false);
 
   useEffect(() => {
